123a228f4bea488da55ab4a996b06a8598fd237a
lpiem <lpiem@iutabgdin202-02.local>
2018-10-15 16:49:13 +0200

    truc

--- WebService/index.php
+++ WebService/index.php
@@ -1,7 +1,2 @@
 <?php
-// web/index.php
-$loader = require_once __DIR__.'/../vendor/autoload.php';
-require __DIR__.'/app.php';
-require __DIR__.'/routes.php';
-
-$app->run();
+echo "Coucou tu veux voir ma...";
