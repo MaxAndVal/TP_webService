66a44d0d604be0007f384e90c03bf7fac68b248e
zigoval <rouby.v@hotmail.com>
2018-11-10 20:30:50 +0100

    remove webService with php and add web service with nodejs

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// web/index.php
-$loader = require_once __DIR__.'./WebService/vendor/autoload.php';
-require __DIR__.'./WebService/src/app.php';
-//require __DIR__.'/../src/routes.php';
-require __DIR__.'./WebService/config/dev.php';
-
-$app->run();
