a3bc177373f24c557437fcf0ae5172f379518faa
MALOSSE MAXIME p1713335 <maxime.malosse@etu.univ-lyon1.fr>
2019-01-31 10:57:41 +0100

    test/ erase test for generating automitic body for interface

--- RickAndMortyAPP/app/src/main/java/com/example/lpiem/rickandmortyapp/Presenter/BaseManager.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lpiem.rickandmortyapp.Presenter
-
-import com.example.lpiem.rickandmortyapp.Data.CallRetrofitInterface
-
-abstract class BaseManager : CallRetrofitInterface {
-
-
-}
