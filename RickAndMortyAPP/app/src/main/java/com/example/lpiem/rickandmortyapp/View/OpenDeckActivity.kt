615d815ee711c116a66dc7d9fb3faffda31a095b
MALOSSE MAXIME p1713335 <maxime.malosse@etu.univ-lyon1.fr>
2019-02-10 10:15:01 +0100

    fix/ added interface

--- RickAndMortyAPP/app/src/main/java/com/example/lpiem/rickandmortyapp/View/OpenDeckActivity.kt
+++ RickAndMortyAPP/app/src/main/java/com/example/lpiem/rickandmortyapp/View/OpenDeckActivity.kt
@@ -1,34 +1,51 @@
 package com.example.lpiem.rickandmortyapp.View
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.lpiem.rickandmortyapp.Model.Card
 import com.example.lpiem.rickandmortyapp.Presenter.OpenDeckManager
 import com.example.lpiem.rickandmortyapp.R
-
 import kotlinx.android.synthetic.main.activity_open_deck.*
 
-class OpenDeckActivity : AppCompatActivity() {
+class OpenDeckActivity : AppCompatActivity(), OpenDecksInterface {
 
-    var openDeckManager : OpenDeckManager? = null
-    var deckOpened : List<Card>? = null
+
+
+    var openDeckManager: OpenDeckManager? = null
+    var deckOpened: List<Card>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_open_deck)
         openDeckManager = OpenDeckManager.getInstance(this)
-        iv_closeOpenDeck.setOnClickListener { onBackPressed() }
+        iv_closeOpenDeck.setOnClickListener { finish() }
 
-        tv_openYourDeck.text = "You have ${openDeckManager!!.loginAppManager.connectedUser!!.deckToOpen} deck to open"
-        iv_peaceAmongWorld.setOnClickListener { openDeckManager!!.openRandomDeck(openDeckManager!!.loginAppManager.connectedUser!!.deckToOpen) }
     }
 
     override fun onResume() {
         super.onResume()
         tv_openYourDeck.text = "You have ${openDeckManager!!.loginAppManager.connectedUser!!.deckToOpen} deck to open"
+        iv_peaceAmongWorld.setOnClickListener { openDeckManager!!.openRandomDeck(openDeckManager!!.loginAppManager.connectedUser!!.deckToOpen, this) }
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
+    override fun showAnimation(show: Boolean) {
+        if (show) {
+            fl_DeckToOpen.visibility = View.GONE
+            tv_openYourDeck.setOnClickListener { }
+            fl_animation.visibility = View.VISIBLE
+            av_from_code.setAnimation("portal_loop.json")
+            av_from_code.playAnimation()
+            av_from_code.loop(true)
+        } else {
+            av_from_code.pauseAnimation()
+            fl_animation.visibility = View.GONE
+            fl_DeckToOpen.visibility = View.VISIBLE
+        }
     }
+
+    override fun updateDecksCount(newCount: Int) {
+        tv_openYourDeck.text = "You have $newCount deck to open"
+    }
+
 }
