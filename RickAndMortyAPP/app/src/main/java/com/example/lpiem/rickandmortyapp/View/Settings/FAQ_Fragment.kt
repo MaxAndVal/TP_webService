f657607e78a52e654c373e32800837e6e653c3e2
MALOSSE MAXIME p1713335 <maxime.malosse@etu.univ-lyon1.fr>
2019-02-23 14:37:18 +0100

    fix/ moving methos from mabager to fragment

--- RickAndMortyAPP/app/src/main/java/com/example/lpiem/rickandmortyapp/View/Settings/FAQ_Fragment.kt
+++ RickAndMortyAPP/app/src/main/java/com/example/lpiem/rickandmortyapp/View/Settings/FAQ_Fragment.kt
@@ -5,33 +5,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.lpiem.rickandmortyapp.Model.FAQ
-import com.example.lpiem.rickandmortyapp.Model.User
 import com.example.lpiem.rickandmortyapp.Manager.settings.FaqManager
 import com.example.lpiem.rickandmortyapp.Manager.settings.SettingsManager
 import com.example.lpiem.rickandmortyapp.R
 import com.example.lpiem.rickandmortyapp.Util.SingletonHolder
+import com.example.lpiem.rickandmortyapp.View.BottomActivity
+import kotlinx.android.synthetic.main.activity_bottom.*
 import kotlinx.android.synthetic.main.fragment_faq.*
 
 class FAQ_Fragment : androidx.fragment.app.Fragment(){
 
     private lateinit var faqManager: FaqManager
-    var listOfFAQ : List<FAQ>?=null
-
-    internal var user: User? = null
 
     companion object : SingletonHolder<SettingsManager, Context>(::SettingsManager)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
+
         faqManager = FaqManager.getInstance(context!!)
-        if (faqManager.faqFragment == null) {
-            faqManager.captureFragmentInstance(this)
-        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -44,11 +38,23 @@ class FAQ_Fragment : androidx.fragment.app.Fragment(){
         super.onViewCreated(view, savedInstanceState)
         rv_faq.layoutManager = LinearLayoutManager(context)
         faqManager.captureRecyclerView(rv_faq)
-        iv_closeFAQ.setOnClickListener { faqManager.closeFAQ(this) }
+        iv_closeFAQ.setOnClickListener { closeFAQ(this) }
+    }
+
+    private fun closeFAQ(fragment: Fragment) {
+        val bottomActivity = (activity as BottomActivity)
+        val fragmentManager = bottomActivity.supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.remove(fragment).commit()
+        bottomActivity.tv_deckToOpen.visibility = View.VISIBLE
+        bottomActivity.tv_wallet.visibility = View.VISIBLE
+        bottomActivity.navigation.visibility = View.VISIBLE
+        bottomActivity.fragmentLayout.visibility = View.VISIBLE
+
     }
 
     override fun onDestroyView() {
-        faqManager.closeFAQ(this)
+        closeFAQ(this)
         super.onDestroyView()
     }
 
