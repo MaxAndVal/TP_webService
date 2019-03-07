51ae5d8c6348ca4abf99ce94a89d568f2a8f92df
MALOSSE MAXIME p1713335 <maxime.malosse@etu.univ-lyon1.fr>
2019-03-07 14:54:03 +0100

    ft/ added Thread management with LivaData

--- RickAndMortyAPP/app/src/main/java/com/example/lpiem/rickandmortyapp/View/Memory/MemoryActivity.kt
+++ RickAndMortyAPP/app/src/main/java/com/example/lpiem/rickandmortyapp/View/Memory/MemoryActivity.kt
@@ -1,15 +1,18 @@
 package com.example.lpiem.rickandmortyapp.View.Memory
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Handler
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import com.example.lpiem.rickandmortyapp.Manager.LoginAppManager
 import com.example.lpiem.rickandmortyapp.Manager.MemoryGameManager
+import com.example.lpiem.rickandmortyapp.Model.ListOfCards
 import com.example.lpiem.rickandmortyapp.Model.Tile
 import com.example.lpiem.rickandmortyapp.R
 import com.example.lpiem.rickandmortyapp.Util.observeOnce
@@ -22,11 +25,13 @@ class MemoryActivity : AppCompatActivity() {
     private val loginAppManager = LoginAppManager.getInstance(this)
     private lateinit var turnObserver: Observer<Int>
     private lateinit var scoreObserver: Observer<Int>
-    private lateinit var initListeners : Observer<MutableList<Tile>>
+    private lateinit var initListeners: Observer<MutableList<Tile>>
     private var displayTurn = MutableLiveData<Int>()
     private var displayScore = MutableLiveData<Int>()
     private var imageViewsToListen = MutableLiveData<MutableList<Tile>>()
     private var rewardsLiveData = MutableLiveData<MutableList<String>>()
+    private var drawLiveData = MutableLiveData<Pair<MutableList<Drawable?>, ListOfCards>>()
+    private lateinit var lisOfImageView: List<ImageView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,12 +42,30 @@ class MemoryActivity : AppCompatActivity() {
         here for it before doing anything or changing text for tv_loading in
         "Vous avez déjà joué aujourd'hui !!" */
 
-        rewardsLiveData.observeOnce( Observer { rewards ->
+        rewardsLiveData.observeOnce(Observer { rewards ->
             Toast.makeText(this,
                     String.format(getString(R.string.memory_game_over),
                             memoryGameManager.score,
                             rewards.toFormattedString()),
                     Toast.LENGTH_LONG).show()
+        })
+
+        drawLiveData.observeOnce(Observer {
+                for ((i, draw) in it.first.withIndex()) {
+                    if (draw == null) {
+                        Toast.makeText(this, getString(R.string.error_fetching_pictures),
+                                Toast.LENGTH_LONG).show()
+                        memoryGameManager.interruptThread()
+                        break
+                    } else {
+                        memoryGameManager.list.add(Pair(draw, it.second.cards!![i].cardName!!))
+                        if (memoryGameManager.list.size == 6) {
+                            memoryGameManager.initGame(lisOfImageView, imageViewsToListen, memoryGameManager.list)
+                            memoryGameManager.interruptThread()
+                            break
+                        }
+                    }
+                }
         })
 
         turnObserver = Observer { currentTurn ->
@@ -67,10 +90,10 @@ class MemoryActivity : AppCompatActivity() {
         tv_turn.text = String.format(getString(R.string.turn_lefts), memoryGameManager.turn)
         tv_memory_score.text = String.format(getString(R.string.current_score), memoryGameManager.score)
 
-        val lisOfImageView = listOf(iv_1, iv_2, iv_3, iv_4, iv_5, iv_6, iv_7, iv_8, iv_9, iv_10,
+        lisOfImageView = listOf(iv_1, iv_2, iv_3, iv_4, iv_5, iv_6, iv_7, iv_8, iv_9, iv_10,
                 iv_11, iv_12)
 
-        memoryGameManager.initCardList(6, lisOfImageView, imageViewsToListen, rewardsLiveData)
+        memoryGameManager.initCardList(6, lisOfImageView, imageViewsToListen, rewardsLiveData, drawLiveData)
     }
 
     private fun setAnimationListener(listOfTiles: MutableList<Tile>) {
@@ -97,6 +120,7 @@ class MemoryActivity : AppCompatActivity() {
             }
         }
     }
+
 
     // MutableList<String> extension
     private fun MutableList<String>.toFormattedString(): String {
