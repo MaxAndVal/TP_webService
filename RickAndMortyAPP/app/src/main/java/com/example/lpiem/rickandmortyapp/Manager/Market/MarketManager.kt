db9b613f73fee4ab49582b84c2e5d82e188bbf23
MALOSSE MAXIME p1713335 <maxime.malosse@etu.univ-lyon1.fr>
2019-03-13 13:13:34 +0100

    fix/ done for market

--- RickAndMortyAPP/app/src/main/java/com/example/lpiem/rickandmortyapp/Manager/Market/MarketManager.kt
+++ RickAndMortyAPP/app/src/main/java/com/example/lpiem/rickandmortyapp/Manager/Market/MarketManager.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import com.example.lpiem.rickandmortyapp.Data.RetrofitCallTypes
 import com.example.lpiem.rickandmortyapp.Data.RickAndMortyRetrofitSingleton
 import com.example.lpiem.rickandmortyapp.Data.SUCCESS
 import com.example.lpiem.rickandmortyapp.Model.Card
@@ -15,25 +14,18 @@ import com.example.lpiem.rickandmortyapp.Model.User
 import com.example.lpiem.rickandmortyapp.R
 import com.example.lpiem.rickandmortyapp.Util.SingletonHolder
 import com.example.lpiem.rickandmortyapp.Util.observeOnce
-import com.example.lpiem.rickandmortyapp.View.Collection.list.CardListDisplay
 import com.example.lpiem.rickandmortyapp.View.Market.MarketActivity
 import com.example.lpiem.rickandmortyapp.View.TAG
-import com.google.gson.JsonObject
-import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MarketManager private constructor(private val context: Context) {
 
     internal var rickAndMortyAPI = RickAndMortyRetrofitSingleton.getInstance(context)
     private var currentCall: Call<*>? = null
-    lateinit var cardListDisplay: CardListDisplay
+    var cardListDisplay = MutableLiveData<ListOfCards>()
     private var marketLiveData = MutableLiveData<ListOfCards>()
     private var marketResponseLiveData = MutableLiveData<ResponseFromApi>()
-
-
-    var friendId = -1
+    private var friendId = -1
 
     companion object : SingletonHolder<MarketManager, Context>(::MarketManager)
 
@@ -42,13 +34,11 @@ class MarketManager private constructor(private val context: Context) {
         currentCall?.cancel()
         Log.d(TAG, "call canceled")
     }
-    //listOfCardTreatment
-    //buyCardTreatment
 
     private fun buyCardTreatment(responseFromApi: ResponseFromApi) {
         if(responseFromApi.code==200){
             Toast.makeText(context, "Congrats, you got a new card", Toast.LENGTH_SHORT).show()
-            getMarket(responseFromApi.results, cardListDisplay, friendId)
+            getMarket(responseFromApi.results, friendId)
         }else{
             Toast.makeText(context, "Error :"+responseFromApi.message, Toast.LENGTH_SHORT).show()
         }
@@ -58,14 +48,13 @@ class MarketManager private constructor(private val context: Context) {
         (context as MarketActivity).listOfCards = response
         val list = context.listOfCards
         if (list?.code == SUCCESS) {
-            cardListDisplay.displayResult(list)
+            cardListDisplay.postValue(list)
         } else {
             Toast.makeText(context, String.format(context.getString(R.string.code_message), list?.code, list?.message), Toast.LENGTH_SHORT).show()
         }
     }
 
-    fun getMarket(user: User?, link: CardListDisplay, friendId: Int?) {
-        cardListDisplay = link
+    fun getMarket(user: User?, friendId: Int?) {
         if (friendId != null) {
             this.friendId = friendId
         }
