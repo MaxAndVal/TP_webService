8995968ee8705a8320f7d5f41d3dcb3a8b6c7d4d
MALOSSE MAXIME p1713335 <maxime.malosse@etu.univ-lyon1.fr>
2019-03-15 08:10:24 +0100

    ft/ rename vars

--- RickAndMortyAPP/app/src/main/java/com/example/lpiem/rickandmortyapp/View/Social/SocialFragment.kt
+++ RickAndMortyAPP/app/src/main/java/com/example/lpiem/rickandmortyapp/View/Social/SocialFragment.kt
@@ -41,69 +41,7 @@ class SocialFragment : androidx.fragment.app.Fragment() {
         loginAppManager = LoginAppManager.getInstance(context!!)
         socialManager = SocialManager.getInstance(context!!)
 
-        updateListObserver = Observer {
-            updateDataSetList(it)
-        }
-
-        changeBtnActionObserver = Observer { btnLabel ->
-            if (btnLabel == LIST_OF_FRIENDS) {
-                tv_list_title?.text = getString(R.string.list_of_friends_title)
-                btn_friendsRequest?.text = getString(R.string.btn_pending_friend_requests)
-                btn_friendsRequest?.setOnClickListener {
-                    socialManager.friendsRequest()
-                    loaderIsVisible(true)
-                }
-            } else if (btnLabel == LIST_OF_FRIENDS_REQUESTS) {
-                tv_list_title?.text = getString(R.string.requests_of_friends_title)
-                btn_friendsRequest?.text = getString(R.string.btn_list_of_friends)
-                btn_friendsRequest?.setOnClickListener {
-                    socialManager.getListOfFriends(loginAppManager.connectedUser!!.userId!!)
-                    loaderIsVisible(true)
-                }
-            }
-        }
-
-        adapterTouchItemObserver = Observer { item ->
-            val (action, friend) = item
-            when (action) {
-                SocialListenerAction.ADD_FRIENDS -> {
-                    Log.d(TAG, item.toString())
-                    if (item.second.accepted == null) {
-                        socialManager.callForAddFriend(friend)
-                        loaderIsVisible(true)
-                    } else {
-                        socialManager.callForValidateFriend(friend)
-                        loaderIsVisible(true)
-                    }
-                }
-                SocialListenerAction.DEL_FRIENDS -> {
-                    val builder: AlertDialog.Builder = AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_NoActionBar)
-
-                    builder.setTitle(getString(R.string.delete_a_friend))
-                            .setMessage(String.format(getString(R.string.sure_to_delete_friend), friend.userName))
-                            .setPositiveButton(android.R.string.yes) { dialog, which ->
-                                socialManager.callToDelFriend(item.second)
-                                loaderIsVisible(true)
-                            }
-                            .setNegativeButton(android.R.string.no) { dialog, which ->
-                                // do nothing
-                            }
-                            .setIcon(android.R.drawable.ic_dialog_alert)
-                            .show()
-
-                }
-                SocialListenerAction.OPEN_FRIEND_MARKET -> {
-                    val intent = Intent(context, MarketActivity::class.java)
-                    intent.putExtra("friend_id", friend.userId)
-                    startActivity(intent)
-                }
-            }
-        }
-
-        loaderObserver = Observer { isVisible ->
-            loaderIsVisible(isVisible)
-        }
-
+        initObservers()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -122,17 +60,17 @@ class SocialFragment : androidx.fragment.app.Fragment() {
         socialManager.getListOfFriends(loginAppManager.connectedUser!!.userId!!)
         btn_searchFriends.setOnClickListener {
             socialManager.searchForFriends(sv_friends.query.toString())
-            loaderIsVisible(true)
+            loaderVisibility(true)
         }
         btn_friendsRequest.setOnClickListener {
             socialManager.friendsRequest()
-            loaderIsVisible(true)
+            loaderVisibility(true)
         }
     }
 
     private fun updateRv() {
         rv_social.adapter?.notifyDataSetChanged()
-        loaderIsVisible(false)
+        loaderVisibility(false)
     }
 
     private fun updateDataSetList(list: List<Friend>?) {
@@ -150,7 +88,7 @@ class SocialFragment : androidx.fragment.app.Fragment() {
 
     }
 
-    private fun loaderIsVisible(isVisible: Boolean) {
+    private fun loaderVisibility(isVisible: Boolean) {
         if (isVisible) {
             social_loading.visibility = VISIBLE
         } else {
@@ -158,13 +96,80 @@ class SocialFragment : androidx.fragment.app.Fragment() {
         }
     }
 
+    private fun initObservers() {
+        updateListObserver = Observer {
+            updateDataSetList(it)
+        }
+
+        changeBtnActionObserver = Observer { btnLabel ->
+            if (btnLabel == LIST_OF_FRIENDS) {
+                tv_list_title?.text = getString(R.string.list_of_friends_title)
+                btn_friendsRequest?.text = getString(R.string.btn_pending_friend_requests)
+                btn_friendsRequest?.setOnClickListener {
+                    socialManager.friendsRequest()
+                    loaderVisibility(true)
+                }
+            } else if (btnLabel == LIST_OF_FRIENDS_REQUESTS) {
+                tv_list_title?.text = getString(R.string.requests_of_friends_title)
+                btn_friendsRequest?.text = getString(R.string.btn_list_of_friends)
+                btn_friendsRequest?.setOnClickListener {
+                    socialManager.getListOfFriends(loginAppManager.connectedUser!!.userId!!)
+                    loaderVisibility(true)
+                }
+            }
+        }
+
+        adapterTouchItemObserver = Observer { item ->
+            val (action, friend) = item
+            when (action) {
+                SocialListenerAction.ADD_FRIENDS -> {
+                    Log.d(TAG, item.toString())
+                    if (item.second.accepted == null) {
+                        socialManager.callForAddFriend(friend)
+                        loaderVisibility(true)
+                    } else {
+                        socialManager.callForValidateFriend(friend)
+                        loaderVisibility(true)
+                    }
+                }
+                SocialListenerAction.DEL_FRIENDS -> {
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_NoActionBar)
+
+                    builder.setTitle(getString(R.string.delete_a_friend))
+                            .setMessage(String.format(getString(R.string.sure_to_delete_friend), friend.userName))
+                            .setPositiveButton(android.R.string.yes) { dialog, which ->
+                                socialManager.callToDelFriend(item.second)
+                                loaderVisibility(true)
+                            }
+                            .setNegativeButton(android.R.string.no) { dialog, which ->
+                                // do nothing
+                            }
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show()
+
+                }
+                SocialListenerAction.OPEN_FRIEND_MARKET -> {
+                    val intent = Intent(context, MarketActivity::class.java)
+                    intent.putExtra("friend_id", friend.userId)
+                    startActivity(intent)
+                }
+            }
+        }
+
+        loaderObserver = Observer { isVisible ->
+            loaderVisibility(isVisible)
+        }
+    }
+
+
+
     override fun onDestroyView() {
         socialManager.updateListLiveData.removeObserver(updateListObserver)
         socialManager.changeBtnActionLiveData.removeObserver(changeBtnActionObserver)
         socialAdapter?.liveDataListener?.removeObserver(adapterTouchItemObserver)
         socialManager.loaderLiveData.removeObserver(loaderObserver)
 
-        loaderIsVisible(false)
+        loaderVisibility(false)
         super.onDestroyView()
     }
 }
